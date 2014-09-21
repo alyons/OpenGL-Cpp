49cb2f10bea4a7da3afc27b33f263d00273515f5
Alexander Lyons <alexander.n.lyons@gmail.com>
2014-09-20 20:13:56 -0400

    Got the Code working

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
-void changeViewport(int w, int h)
-{
+void changeViewport(int w, int h) {
 	glViewport(0, 0, w, h);
+}
+
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main (int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Kyle's so fucking good at what he does.");
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+
+	glutMainLoop();
+	return 0;
 }
